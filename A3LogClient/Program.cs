bab0bc37d03c75a2f1ca0ed429e9a8d9fddded7b
Joseph <Jcarson3035@conestogac.on.ca>
2024-02-16 12:37:56 -0500

    Added simple tcp connection.

--- A3LogClient/Program.cs
+++ A3LogClient/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,25 @@ namespace A3LogClient
     {
         static void Main(string[] args)
         {
+
+            string serverIp = "10.0.0.60";
+            int serverPort = 30001;
+            string logMessage = "test";
+
+            try
+            {
+                using (TcpClient client = new TcpClient(serverIp, serverPort))
+                using (NetworkStream stream = client.GetStream())
+                {
+                    byte[] data = Encoding.UTF8.GetBytes(logMessage);
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Log message sent successfully.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
         }
     }
 }
--- A3LogClient/TestCases.cs
+++ A3LogClient/TestCases.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Not sure the test cases to make yet
+
 namespace A3LogClient
 {
     internal class TestCases
